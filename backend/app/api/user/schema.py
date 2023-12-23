7ff340caae0fde7a3747461f970f9abe51928cf5
Bussler <maarten.bussler@gmail.com>
2023-12-23 14:51:08 +0100

    add sql apis

---
+++ backend/app/api/user/schema.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class UserSchema(BaseModel):
+    name: str
+    surname: str
+
+    model_config: ConfigDict(from_config="config")
--- backend/app/api/user/user_api.py
+++ backend/app/api/user/user_api.py
@@ -1,20 +1,58 @@
-from typing import Optional
+from typing import List, Optional
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.orm import selectinload
 
+from api.user.schema import UserSchema
+from db.models.model_export import DBUser
 from db import AsyncSession, get_session
 
 user_router = APIRouter(prefix="/user", tags=["user"])
 
 
-@user_router.get("")
+@user_router.get("", response_model=List[UserSchema])
 async def get(
     name: Optional[str] = None,
     surname: Optional[str] = None,
+    limit: int = 100,
     db_session: AsyncSession = Depends(get_session),
-):
-    return {"user": "user"}
+) -> List[UserSchema]:
+    stmt = select(DBUser)
+
+    if name:
+        stmt = stmt.where(DBUser.name == name)
+
+    if surname:
+        stmt = stmt.where(DBUser.surname == surname)
+
+    users = await db_session.scalar(stmt.limit(limit))
+
+    return [UserSchema(**user) for user in users]
+
+
+@user_router.post("/create")
+async def create(
+    name: Optional[str] = None,
+    surname: Optional[str] = None,
+    db_session: AsyncSession = Depends(get_session),
+) -> UserSchema:
+    user = await db_session.scalar(
+        select(DBUser).where(DBUser.name == name).where(DBUser.surname == surname)
+    )
+
+    if user is not None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User already exists",
+        )
+
+    new_user = DBUser(name=name, surname=surname)
+
+    db_session.add(new_user)
+    await db_session.commit()
+    await db_session.refresh(new_user)
+
+    return UserSchema(**new_user)
 
 
 @user_router.get("/greeting")
